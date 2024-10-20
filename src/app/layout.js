2fb31fd81bafe4325be4aec7598ef5845eb115c6
zaheenrakib <rakibislambest@gmail.com>
2024-10-20 23:43:12 +0600

    Project setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Heading from "@/components/Heading";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,6 +24,8 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        this is new heading
+        <Heading/>
         {children}
       </body>
     </html>
