2eabb149899b6a673c358527c25447505ab54614
zaheenrakib <rakibislambest@gmail.com>
2024-10-28 12:46:12 +0600

    Create Navbar with Dynamic routing  and active routing also

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,48 @@
+"use client"
+import Link from 'next/link'
+import { usePathname, useRouter } from 'next/navigation'
 import React from 'react'
 
 const Navbar = () => {
+  const pathName = usePathname();
+  const router = useRouter();
+  const navLinks = [
+    {
+      path: "/",
+      title: "Home"
+    },
+    {
+      path: "/about",
+      title: "About"
+    },
+    {
+      path: "/contact",
+      title: "Contact"
+    },
+    {
+      path: "/news",
+      title: "News"
+    },
+    {
+      path: "/blog",
+      title: "Blog"
+    },
+  ]
+  const hadler = () => {
+    router.push('/login')
+  }
   return (
-    <div>
-      <nav className="bg-red-500">
-        <ul className="flex justify-center items-center gap-5">
-          <Link href="/" ><li>Home</li></Link>
-          <Link href="/about" > <li>About</li></Link>
-          <Link href="/contact" ><li>Contact</li></Link>
-          <Link href="/news" ><li>News</li></Link>
-          <Link href="/blog" ><li>Blog</li></Link>
-          <Link href="/login" ><li>Login</li></Link>
+    <div className='bg-sky-600'>
+      <nav className="container mx-auto flex justify-between ">
+        <h6 className='text-3xl'>Next <span className='text-emerald-200'>Hero</span></h6>
+        <ul className="flex justify-center items-center gap-5 text-2xl">
+          {
+            navLinks?.map((link) => <Link className={`${pathName === link.path && "text-white"}`} key={link.path} href={link.path} > {link.title} </Link>)
+          }
         </ul>
-      </nav></div>
+        <button onClick={hadler} className='btn btn-primary'>Login</button>
+      </nav>
+    </div>
   )
 }
 
